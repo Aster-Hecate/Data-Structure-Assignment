5c55f0ed47f8e98c1909cf189d6aa42cc9686fb7
Skuld Virva <Frozenrefler@hotmail.com>
2021-01-04 12:22:00 +0800

    settled

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<sstream>
+#define HEADERS_READY
+
+#include "lib/requisitories.h"
+#include "lib/graph_class.cpp"
+#include "lib/dijkstra.cpp"
+
+using namespace std;
+
+int main() {
+  
+  ifstream IF;
+  IF.open("graph/test");
+  Graph<int> g;
+  g.init(IF);
+  unitSet* uspt = optimize(g, "A");
+  IF.close();
+  delete[] uspt;
+  
+}
